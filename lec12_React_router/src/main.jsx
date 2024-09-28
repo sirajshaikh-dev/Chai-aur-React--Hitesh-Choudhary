901c7ddae02179058c5fcfde1f6f8ef605b4c005
Siraj <ss1425306@gmail.com>
2024-09-28 21:24:25 +0530

    Add lec12:React router

---
+++ lec12_React_router/src/main.jsx
@@ -0,0 +1,53 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
+
+/*Method 1 */
+const router= createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"about",
+        element: <About/>
+      },
+      {
+        path:"contact",
+        element: <Contact/>
+      },
+      {
+        path:"user/:userid",
+        element: <User/>
+      },
+    ]
+  }
+])
+
+/*Method 2 */
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout/>}> 
+//       <Route path='' element={<Home/>} /> 
+//       <Route path='about' element={<About/>} /> 
+//       <Route path='contact' element={<Contact/>} /> 
+//       {/* <Route path='user/:userid' element={<User/>} />  */}
+//     </Route> 
+//   )
+// )
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router}/>
+  </StrictMode>,
+)
