9499274ced67f48f984f200693c4aa47f2a37525
Siraj <ss1425306@gmail.com>
2024-09-30 21:49:18 +0530

    Add lec13 PART1 CONTEXT API

---
+++ lec13(1)_Context_api/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import UserContextProvider from './Context/UserContextProvider'
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+       <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
