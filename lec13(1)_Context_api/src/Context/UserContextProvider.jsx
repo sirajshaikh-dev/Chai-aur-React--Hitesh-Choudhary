1a9aade001957da82472c481976b8e38d81ae55d
Siraj <ss1425306@gmail.com>
2024-10-01 13:12:49 +0530

    Update file path

--- lec13(1)_Context_api/src/Context/UserContextProvider.jsx
+++ lec13(1)_Context_api/src/Context/UserContextProvider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserContext from "./UserContext";
+import UserContext from "./Context";
 
 const UserContextProvider = ({children}) => {
     const [user, setUser] = React.useState(null)
