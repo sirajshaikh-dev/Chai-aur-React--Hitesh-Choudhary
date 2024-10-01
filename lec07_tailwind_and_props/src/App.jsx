3ddcb45221b4596b2b86cdb7df2fce7f9a8e4ef7
Siraj <ss1425306@gmail.com>
2024-10-01 12:25:44 +0530

    Modify Cards and add styling

--- lec07_tailwind_and_props/src/App.jsx
+++ lec07_tailwind_and_props/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import Card from './components/card'
-
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
 // let obj={
 //   username: "siraj",
 //   age:21,
@@ -9,16 +8,24 @@ import Card from './components/card'
 // let arr=[1,2,3,4]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-3xl text-red-400 font-bold underline bg-green-300 rounded-2xl'>Tailwind and Props</h1>
+      <h1 className="text-3xl text-red-400 font-bold underline bg-green-300 rounded-2xl">
+        Tailwind and Props
+      </h1>
       {/* <Card name= "siraj"  {...obj} arr={arr}/> */}
-      <Card username="Sundar" btnText="Visit" imgUrl="https://img-cdn.inc.com/image/upload/w_600,ar_16:9,c_fill,g_auto,q_auto:best/images/panoramic/sundar-pichai-google-inc-1493186230_538103_o987ze.webp"/>
+      <div className="flex justify-normal flex-wrap gap-6 mt-6">
       <Card username="Natalie Paisley" />
-     </>
-  )
+      <Card
+          username="Sundar"
+          btnText="Visit"
+          imgUrl="https://img-cdn.inc.com/image/upload/w_600,ar_16:9,c_fill,g_auto,q_auto:best/images/panoramic/sundar-pichai-google-inc-1493186230_538103_o987ze.webp"
+          Position="CEO-Google"
+          about={"Sundar Pichai is the CEO of Alphabet Inc. and its subsidiary Google. He has played a pivotal role in driving Google's growth and innovation, overseeing the development of key products like Google Chrome and Android."}
+      />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
