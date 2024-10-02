0711ff54d05ababe75e62597be1494e2a07ce953
Siraj <ss1425306@gmail.com>
2024-10-02 12:06:37 +0530

    update Usercontext import issue

--- lec13(1)_Context_api/src/Context/UserContextProvider.jsx
+++ lec13(1)_Context_api/src/Context/UserContextProvider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserContext from "./Context";
+import UserContext from "./Usercontext";
 
 const UserContextProvider = ({children}) => {
     const [user, setUser] = React.useState(null)
